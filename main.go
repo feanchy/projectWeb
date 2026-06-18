ac5b338aad200be61829ad347cb09ad209e2ada0
feanchy <mgusejn007@gmail.com>
2026-06-18 23:16:15 +0300

    add register route

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Home page")
 }
 
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Register page")
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Login page")
 }
@@ -18,6 +22,7 @@ func main() {
 
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/login", loginHandler)
+	mux.HandleFunc("/register", registerHandler)
 
 	fmt.Println("Server started on http://localhost:8080")
 	http.ListenAndServe(":8080", mux)
